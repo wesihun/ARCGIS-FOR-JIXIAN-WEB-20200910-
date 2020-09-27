40d15b3d53efb8181cc852f8210334aa79fd4baf
wesihun <1498731041@qq.com>
2020-09-27 14:05:00 +0800

    init

--- 源代码/src/main/java/com/wt/arcgis/controller/MyController.java
+++ 源代码/src/main/java/com/wt/arcgis/controller/MyController.java
@@ -808,14 +808,14 @@ public class MyController {
     }
 
     @RequestMapping(value = "getPhysicsServiceByUpdatetime", produces = "application/json;charset=utf-8")
-    public TB_PHYSICSTABLE getPhysicsServiceByUpdatetime(String updatetime, int type,String physicstable) {//根据更新时间取得地类图斑服务
+    public TB_PHYSICSTABLE getPhysicsServiceByUpdatetime(String updatetime, int type,String physicstable) {//根据更新时间取得xx服务
         if(null==updatetime || "".equals(updatetime)) return null;
 
         TB_PHYSICSTABLE tb_physicstable = myMapper.getPhysicsServiceByUpdatetime(updatetime, type,physicstable);
         return tb_physicstable;
     }
     @RequestMapping(value = "getAllPhysicsServiceVersion", produces = "application/json;charset=utf-8")
-    public List<TB_PHYSICSTABLE> getAllPhysicsServiceVersion(int type,String physicstable) {//取得所有地类图斑服务版本日期
+    public List<TB_PHYSICSTABLE> getAllPhysicsServiceVersion(int type,String physicstable) {//取得所有xx服务版本日期
         List<TB_PHYSICSTABLE> tb_physicstableList = myMapper.getAllPhysicsServiceVersion(type,physicstable);
         return tb_physicstableList;
     }
